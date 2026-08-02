d4031ca07f6e17c9b27d8e7387d1c6100f915868
kiLeo13 <leonardo.silvavieira@hotmail.com>
2026-08-02 17:44:48 -0300

    Remove shared inventory domain and storage

--- src/main/java/sld/saladafun/shared/inventory/model/InitialInventoryMode.java
+++
@@ -1,7 +0,0 @@
-package sld.saladafun.shared.inventory.model;
-
-public enum InitialInventoryMode {
-    EMPTY,
-    SOURCE_PLAYER,
-    RESUMED
-}
--- src/main/java/sld/saladafun/shared/inventory/model/SessionStatus.java
+++
@@ -1,6 +0,0 @@
-package sld.saladafun.shared.inventory.model;
-
-public enum SessionStatus {
-    ACTIVE,
-    ARCHIVED
-}
