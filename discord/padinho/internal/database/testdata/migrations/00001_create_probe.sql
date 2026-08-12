9cc1ce11b1ec8b8140266198b1c7a915155a76d6
kiLeo13 <leonardo.silvavieira@hotmail.com>
2026-08-11 21:38:53 -0300

    Remove migrations from Padinho

--- discord/padinho/internal/database/testdata/migrations/00001_create_probe.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
-CREATE TABLE migration_probe (
-    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    name VARCHAR(64) NOT NULL,
-    PRIMARY KEY (id)
-);
-
--- +goose Down
-DROP TABLE migration_probe;
