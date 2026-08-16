f58b3ca56b105d5ad311d6df3c6c5566412f6826
kiLeo13 <leonardo.silvavieira@hotmail.com>
2026-08-16 20:17:02 -0300

    Clarify MySQL module contracts

--- infrastructure/terraform/modules/mysql/outputs.tf
+++ infrastructure/terraform/modules/mysql/outputs.tf
@@ -1,2 +1,9 @@
-output "db_system_id" { value = oci_mysql_mysql_db_system.this.id }
-output "private_ip" { value = oci_mysql_mysql_db_system.this.ip_address }
+output "db_system_id" {
+  description = "OCID of the MySQL DB system."
+  value       = oci_mysql_mysql_db_system.this.id
+}
+
+output "private_ip" {
+  description = "Private IPv4 address of the MySQL DB system."
+  value       = oci_mysql_mysql_db_system.this.ip_address
+}
--- infrastructure/terraform/modules/mysql/variables.tf
+++ infrastructure/terraform/modules/mysql/variables.tf
@@ -1,24 +1,55 @@
-variable "compartment_ocid" { type = string }
-variable "availability_domain" { type = string }
-variable "subnet_id" { type = string }
-variable "nsg_ids" { type = list(string) }
-variable "name" { type = string }
-variable "admin_username" { type = string }
-variable "admin_password" {
-  type      = string
-  sensitive = true
+variable "compartment_ocid" {
+  description = "OCID of the compartment that owns the DB system."
+  type        = string
 }
+
+variable "availability_domain" {
+  description = "Tenancy-specific availability-domain name for MySQL."
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "OCID of the private subnet that hosts MySQL."
+  type        = string
+}
+
+variable "nsg_ids" {
+  description = "Network security groups attached to MySQL."
+  type        = list(string)
+}
+
+variable "name" {
+  description = "Display name assigned to the DB system."
+  type        = string
+}
+
+variable "admin_username" {
+  description = "Administrator username created with the DB system."
+  type        = string
+}
+
+variable "admin_password" {
+  description = "Administrator password created with the DB system."
+  type        = string
+  sensitive   = true
+}
+
 variable "database_shape" {
-  type    = string
-  default = "MySQL.Free"
+  description = "Always Free shape used by the MySQL DB system."
+  type        = string
+  default     = "MySQL.Free"
+
   validation {
     condition     = var.database_shape == "MySQL.Free"
     error_message = "The database shape must remain MySQL.Free."
   }
 }
+
 variable "heatwave_shape" {
-  type    = string
-  default = "HeatWave.Free"
+  description = "Always Free shape used by the HeatWave cluster."
+  type        = string
+  default     = "HeatWave.Free"
+
   validation {
     condition     = var.heatwave_shape == "HeatWave.Free"
     error_message = "The HeatWave shape must remain HeatWave.Free."
