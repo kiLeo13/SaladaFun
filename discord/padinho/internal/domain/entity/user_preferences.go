39b51611fee45ad64cde2adc8e6c36cdb72ef731
kiLeo13 <leonardo.silvavieira@hotmail.com>
2026-08-26 23:54:12 -0300

    Store the Ouroquest user preference

--- discord/padinho/internal/domain/entity/user_preferences.go
+++ discord/padinho/internal/domain/entity/user_preferences.go
@@ -4,6 +4,7 @@ package entity
 type UserPreferences struct {
 	UserID      uint64 `gorm:"column:user_id;primaryKey;autoIncrement:false"`
 	AutoMudaeOC *bool  `gorm:"column:auto_mudae_oc"`
+	AutoMudaeOQ *bool  `gorm:"column:auto_mudae_oq"`
 	CreatedAt   int64  `gorm:"column:created_at;autoCreateTime:milli"`
 	UpdatedAt   int64  `gorm:"column:updated_at;autoUpdateTime:milli"`
 }
